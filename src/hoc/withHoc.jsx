d7179a1474de450716884b3933c0a9d7b8b7c106
Lapitskyi <lapitskiy4@gmail.com>
2021-04-24 20:48:52 +0300

    refactoring hoc(useContext)

--- src/hoc/withHoc.jsx
+++ src/hoc/withHoc.jsx
@@ -1,41 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import StoreContext from '../store/StoreContext';
 import MyContext from '../store/MyContext';
 
 const withHoc = (Component) => {
-  return class extends React.Component {
-    render() {
-      return (
-        <StoreContext.Consumer>
-          {
-            (storeT) => (
-              <MyContext.Consumer>
-                {
-                  ({
-                    theme,
-                    toggleTheme,
-                    lang,
-                    langToggle,
-                  }) => (
-                    <Component
-                      storeT={storeT}
-                      updateTheme={{
-                        theme,
-                        toggleTheme
-                      }}
-                      toggleLang={{
-                        lang,
-                        langToggle
-                      }}
-                    />
-                  )
-                }
-              </MyContext.Consumer>
-            )
-          }
-        </StoreContext.Consumer>
-      );
-    }
+  return () => {
+    const storeT = useContext(StoreContext);
+    const {
+      theme,
+      toggleTheme,
+      lang,
+      langToggle
+    } = useContext(MyContext);
+    return (
+      <Component
+        storeT={storeT}
+        updateTheme={{
+          theme,
+          toggleTheme
+        }}
+        toggleLang={{
+          lang,
+          langToggle
+        }}
+      />
+    );
   };
 };
 
